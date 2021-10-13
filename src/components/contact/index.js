5cc3b80e5ae32057a68558237892af05034e2f95
Sebastian Livoni <seb@livoni.me>
2021-10-13 18:03:36 +0200

    contact form now added validation and soon complete

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -9,8 +9,25 @@ export default function Contact() {
     reply_to: '',
   });
 
+  const [message, setMessage] = useState();
+  const [buttonMessage, setButtonMessage] = useState("Submit");
+  const [showSuccess, setShowSuccess] = useState();
+
   const onSubmit = (e) => {
     e.preventDefault();
+    if (toSend.message == '') {
+      setMessage("Please type a message!");
+      return -1;
+    }
+    if (toSend.from_name == '') {
+      setMessage("Please type your name!");
+      return -1;
+    }
+    if (toSend.reply_to == '') {
+      setMessage("Please type your email!");
+      return -1;
+    }
+    setButtonMessage("Please wait...");
     send(
       'service_cysj1rb',
       'template_5mswnb5',
@@ -19,6 +36,7 @@ export default function Contact() {
     )
       .then((response) => {
         console.log('SUCCESS!', response.status, response.text);
+        setShowSuccess(true);
       })
       .catch((err) => {
         console.log('FAILED...', err);
@@ -37,66 +55,46 @@ export default function Contact() {
             <h2 className="text-3xl font-semibold md:text-4xl lg:text-5xl lg:ml-12 dark:text-white">Want to get in touch?</h2>
           </div>
           <div className="flex-1 mt-10 lg:mt-0">
-            <form className="w-full lg:max-w-md" onSubmit={onSubmit}>
-              <div className="md:flex -ml-3 mb-6 mr-3 md:mr-0">
-                <div className="md:w-1/2 md:mr-3 mb-6 md:mb-0">
-                  <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
-                    First & Last Name
-                  </label>
-                  <input name='from_name' value={toSend.from_name} onChange={handleChange} className="appearance-none block w-full bg-gray-200 dark:bg-custom-dark-black  dark:border-transparent text-gray-700 dark:text-custom-light-gray dark:placeholder-custom-light-gray rounded py-3 px-4 mb-3 leading-tight focus:outline-none" id="grid-name" type="text" placeholder="Enter your name" />
-                  {/*border-red-500*/}
-                  {/*<p className="text-red-500 text-xs italic">Please fill out this field.</p>*/}
-                </div>
-                <div className="md:w-1/2 md:ml-3 mb-6">
-                  <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-email">
-                    Email Adress
-                  </label>
-                  <input name='reply_to' value={toSend.reply_to} onChange={handleChange} className="appearance-none block w-full bg-gray-200 dark:bg-custom-dark-black dark:border-transparent text-gray-700 dark:text-custom-light-gray dark:placeholder-custom-light-gray rounded py-3 px-4 leading-tight focus:outline-none" id="grid-email" type="email" placeholder="Enter your email" />
-                </div>
-              </div>
-              <div className="-ml-3 mb-8 mr-3 md:mr-0">
-                <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-message">
-                  Message
-                </label>
-                <textarea name='message' value={toSend.message} onChange={handleChange} className="appearance-none block w-full bg-gray-200 dark:bg-custom-dark-black dark:border-transparent text-gray-700 dark:text-custom-light-gray dark:placeholder-custom-light-gray rounded py-3 px-4 leading-tight focus:outline-none" id="grid-message" type="text" rows="5" placeholder="Enter your message"></textarea>
-              </div>
-              <button type='submit' className="-ml-3 px-8 py-3 bg-black dark:bg-gray-200 rounded-md duration-300 text-white hover:bg-gray-800 dark:text-black dark:hover:bg-gray-200">Submit</button>
-            </form>
-            {}
+            {!showSuccess ? 
+              <>
+                <form className="w-full lg:max-w-md" onSubmit={onSubmit}>
+                  <div className="md:flex -ml-3 mb-6 mr-3 md:mr-0">
+                    <div className="md:w-1/2 md:mr-3 mb-6 md:mb-0">
+                      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
+                        First & Last Name
+                      </label>
+                      <input name='from_name' value={toSend.from_name} onChange={handleChange} className="appearance-none block w-full bg-gray-200 dark:bg-custom-dark-black  dark:border-transparent text-gray-700 dark:text-custom-light-gray dark:placeholder-custom-light-gray rounded py-3 px-4 mb-3 leading-tight focus:outline-none" id="grid-name" type="text" placeholder="Enter your name" />
+                      {/*border-red-500*/}
+                      {/*<p className="text-red-500 text-xs italic">Please fill out this field.</p>*/}
+                    </div>
+                    <div className="md:w-1/2 md:ml-3 mb-6">
+                      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-email">
+                        Email Adress
+                      </label>
+                      <input name='reply_to' value={toSend.reply_to} onChange={handleChange} className="appearance-none block w-full bg-gray-200 dark:bg-custom-dark-black dark:border-transparent text-gray-700 dark:text-custom-light-gray dark:placeholder-custom-light-gray rounded py-3 px-4 leading-tight focus:outline-none" id="grid-email" type="email" placeholder="Enter your email" />
+                    </div>
+                  </div>
+                  <div className="-ml-3 mb-8 mr-3 md:mr-0">
+                    <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-message">
+                      Message
+                    </label>
+                    <textarea name='message' value={toSend.message} onChange={handleChange} className="appearance-none block w-full bg-gray-200 dark:bg-custom-dark-black dark:border-transparent text-gray-700 dark:text-custom-light-gray dark:placeholder-custom-light-gray rounded py-3 px-4 leading-tight focus:outline-none" id="grid-message" type="text" rows="5" placeholder="Enter your message"></textarea>
+                  </div>
+                  <button type='submit' className="-ml-3 px-8 py-3 bg-black dark:bg-gray-200 rounded-md duration-300 text-white hover:bg-gray-800 dark:text-black dark:hover:bg-gray-200">{buttonMessage}</button>
+                  <p className="-ml-3 mt-5 text-red-500 text-xs italic">{message}</p>
+                </form>
+              </> :
+              <>
+                <h1>Thank you for your message</h1>
+              </>
+            }
           </div>
         </div>
       </div>
-      <form className="mt-16" onSubmit={onSubmit}>
-        <input
-          type='text'
-          name='from_name'
-          placeholder='from name'
-          value={toSend.from_name}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='to_name'
-          placeholder='to name'
-          value={toSend.to_name}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='message'
-          placeholder='Your message'
-          value={toSend.message}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='reply_to'
-          placeholder='Your email'
-          value={toSend.reply_to}
-          onChange={handleChange}
-        />
-        <button type='submit'>Submit</button>
-      </form>
     </>
   )
+}
+
+const Form = () => {
+  
 }
