0f7bc5d41e9dae9e3b45859d23ee124e2758cbe1
Sebastian Livoni <seb@livoni.me>
2021-10-14 13:17:43 +0200

    added programming skills

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,7 +1,25 @@
 export default function Skills() {
   return (
-    <div className="dark:text-white">
+    <div className="dark:text-white my-20">
       <h1 className="text-center text-3xl font-bold mt-10">Programming Skills</h1>
+      <p className="my-7 text-center" >These are the areas that I have experience with.</p>
+      <div className="flex flex-wrap justify-center space-x-4">
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">React</p>
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">Tailwind CSS</p>
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">Bootstrap</p>
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">JavaScript</p>
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">C</p>
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">Overleaf</p>
+        <p className="px-6 py-3 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">Git</p>
+      </div>
+      {/*<div className="bg-red-200 w-full h-96 mt-8 flex space-x-5">
+        <div className="bg-red-500 h-full w-14">
+          <div className="bg-red-700 h-3/4 w-14 rounded-t-xl"></div>
+        </div>
+        <div className="bg-red-500 h-full w-14">
+          <div className="bg-red-700 h-3/4 w-14 rounded-t-xl"></div>
+        </div>
+      </div>*/}
       
       
       {/*<h2>Tailwind CSS</h2>
