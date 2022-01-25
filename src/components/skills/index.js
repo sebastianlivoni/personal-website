a2a6c9633b6b171db9adcf8d668b3067067d7099
Sebastian Livoni <safe.tea7364@livoni.me>
2022-01-25 16:38:47 +0100

    updated with new experience and fixing small key error

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -6,8 +6,8 @@ export default function Skills() {
       <h1 className="text-center text-3xl font-bold mt-10">Programming Skills</h1>
       <p className="my-7 text-center" >These are the areas that I have experience with.</p>
       <div className="flex flex-wrap justify-center space-x-4">
-        {skills.map((item) => (
-          <p className="shadow-xl px-6 py-3 mb-4 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">{item}</p>
+        {skills.map((item, i) => (
+          <p key={i} className="shadow-xl px-6 py-3 mb-4 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">{item}</p>
           ))}
       </div>
     </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,54 @@ import TimeLine from '../components/timeline/index.jsx';
 import AboutMe from '../components/about';
 import Skills from '../components/skills';
 
+const timelinesitems = [
+  {
+    title: "Primary school",
+    desc: "I started my time in primary school in 2007 and graduated in June 2017.",
+    year_from: "2007",
+    year_to: "2017"
+  },
+  {
+    title: "Ingstrup Efterskole (Continuation School)",
+    desc: "After I graduated in 2017, I went to a continuation school just outside Løkken in Denmark called Ingstrup Efterskole. My elective was football, which is why I also played a lot of football during the school year.",
+    year_from: "2017",
+    year_to: "2018"
+  },
+  {
+    title: "Aalborg Business College, Turøgade",
+    desc: "After after school I started at Aalborg Business School. Here I went for 3 years and graduated in June 2021. My electives was focused on Mathematics and International Economics.",
+    year_from: "2018",
+    year_to: "2021"
+  },
+  {
+    title: "Computer Science at Aalborg University",
+    desc: "Programming is my biggest passion and I really want to work with what I love. Therefore I applied to Computer Science at Aalborg University. I was admitted in September 2021 and therefore I am currently studying computer science.",
+    year_from: "2021",
+    year_to: "2026"
+  },
+]
+
+const experience = [
+  {
+    title: "Spar Vestbjerg (Super Market)",
+    desc: "While studying on Business College I was working as Cashier at the local super market.",
+    year_from: "2017",
+    year_to: "2019"
+  },
+  {
+    title: "Pakke.dk",
+    desc: "I'm currently working as a Warehouse Employee and Software Developer at Pakke.dk",
+    year_from: "2019",
+    year_to: "now"
+  },
+  {
+    title: "Perlemust.dk",
+    desc: "I'm currently also working as a Shopify Agent and Web Developer at Perlemust.dk. I am further developing the website with new information and designs.",
+    year_from: "2020",
+    year_to: "now"
+  }
+]
+
 export default function AboutPage() {
   return (
     <>
@@ -14,7 +62,9 @@ export default function AboutPage() {
           <div className="mx-8">
             <AboutMe />
 
-            <TimeLine />
+            <h1 className="text-center text-3xl font-bold dark:text-white">My Short Resume & Journey</h1>
+            <TimeLine title={"Education"} items={timelinesitems} />
+            <TimeLine title={"Experience"} items={experience} />
             <Skills />
           </div>
         </div>
