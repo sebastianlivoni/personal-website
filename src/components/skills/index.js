103361fc17c65bf273d836039f5906fb9b03655f
Sebastian Livoni <sebastian.livoni@icloud.com>
2022-05-30 18:59:21 +0200

    more improvements

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,12 +1,10 @@
-const skills = ["JavaScript", "React", "Tailwind CSS", "Shopify", "Bootstrap", "C", "Git"]
-
-export default function Skills() {
+export default function Skills(props) {
   return (
     <div className="dark:text-white my-20">
-      <h1 className="text-center text-3xl font-bold mt-10">Programming Skills</h1>
-      <p className="my-7 text-center" >These are the areas that I have experience with.</p>
+      <h1 className="text-center text-3xl font-bold mt-10">{props.title}</h1>
+      <p className="my-7 text-center" >{props.desc}</p>
       <div className="flex flex-wrap justify-center space-x-4">
-        {skills.map((item, i) => (
+        {props.skills.map((item, i) => (
           <p key={i} className="shadow-xl px-6 py-3 mb-4 bg-red-400 hover:bg-red-500 cursor-pointer rounded-lg text-sm text-white">{item}</p>
           ))}
       </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,16 +8,10 @@ import Skills from '../components/skills';
 
 const timelinesitems = [
   {
-    title: "Primary school",
-    desc: "I started my time in primary school in 2007 and graduated in June 2017.",
-    year_from: "2007",
-    year_to: "2017"
-  },
-  {
-    title: "Ingstrup Efterskole (Continuation School)",
-    desc: "After I graduated in 2017, I went to a continuation school just outside Løkken in Denmark called Ingstrup Efterskole. My elective was football, which is why I also played a lot of football during the school year.",
-    year_from: "2017",
-    year_to: "2018"
+    title: "Computer Science at Aalborg University",
+    desc: "Programming is my biggest passion and I really want to work with what I love. Therefore I applied to Computer Science at Aalborg University. I was admitted in September 2021 and therefore I am currently studying computer science.",
+    year_from: "2021",
+    year_to: "2026"
   },
   {
     title: "Aalborg Business College, Turøgade",
@@ -26,19 +20,25 @@ const timelinesitems = [
     year_to: "2021"
   },
   {
-    title: "Computer Science at Aalborg University",
-    desc: "Programming is my biggest passion and I really want to work with what I love. Therefore I applied to Computer Science at Aalborg University. I was admitted in September 2021 and therefore I am currently studying computer science.",
-    year_from: "2021",
-    year_to: "2026"
+    title: "Ingstrup Efterskole (Continuation School)",
+    desc: "After I graduated in 2017, I went to a continuation school just outside Løkken in Denmark called Ingstrup Efterskole. My elective was football, which is why I also played a lot of football during the school year.",
+    year_from: "2017",
+    year_to: "2018"
   },
+  {
+    title: "Primary school",
+    desc: "I started my time in primary school in 2007 and graduated in June 2017.",
+    year_from: "2007",
+    year_to: "2017"
+  }
 ]
 
 const experience = [
   {
-    title: "Spar Vestbjerg (Super Market)",
-    desc: "While studying on Business College I was working as Cashier at the local super market.",
-    year_from: "2017",
-    year_to: "2019"
+    title: "Perlemust.dk",
+    desc: "I'm currently also working as a Shopify Agent and Web Developer at Perlemust.dk. I am further developing the website with new information and designs.",
+    year_from: "2020",
+    year_to: "2022"
   },
   {
     title: "Pakke.dk",
@@ -47,17 +47,19 @@ const experience = [
     year_to: "2022"
   },
   {
-    title: "Perlemust.dk",
-    desc: "I'm currently also working as a Shopify Agent and Web Developer at Perlemust.dk. I am further developing the website with new information and designs.",
-    year_from: "2020",
-    year_to: "2022"
+    title: "Spar Vestbjerg (Super Market)",
+    desc: "While studying on Business College I was working as Cashier at the local super market.",
+    year_from: "2017",
+    year_to: "2019"
   }
 ]
+
+const skills = ["JavaScript", "React", "Tailwind CSS", "Shopify", "Bootstrap", "C", "Git"]
+const compentencies = ["IT", "Communication", "Support", "Flexible", "Googling", "Solution-oriented", "Structured", "Targeted", "Self employed", "Teamplayer"]
 
 export default function AboutPage() {
 
   useEffect(() => {
-    // Update the document title using the browser API
     document.title = "Livoni.me | About me";
   });
 
@@ -69,10 +71,13 @@ export default function AboutPage() {
           <div className="mx-8">
             <AboutMe />
 
-            <h1 className="text-center text-3xl font-bold dark:text-white">My Short Resume & Journey</h1>
+            <Skills skills={compentencies} title="Core compentencies" desc={<span>The good old adjectives.</span>} />
+
+            <h1 className="text-center text-3xl font-bold dark:text-white">My Resume & Journey</h1>
             <TimeLine title={"Education"} items={timelinesitems} />
             <TimeLine title={"Experience"} items={experience} />
-            <Skills />
+
+            <Skills skills={skills} title="Programming Skills" desc="These are the areas that I have experience with." />
           </div>
         </div>
         <Contact />
@@ -81,3 +86,5 @@ export default function AboutPage() {
     </>
   );
 }
+
+// {/* If you want to know more, <span className='bg-gradient-to-br bg-clip-text text-transparent from-white to-white from-pink-400 to-red-600 font-bold font-normal'>click</span> on the adjective. /*}
